90ddd341dbe01ea67cbe6f7ac579c11836feed55
SoumyaS09 <shetty.soumya09@yahoo.in>
2024-02-29 12:15:06 +0530

    Create StudentLab6Application.java

---
+++ student_lab6/student_lab6/src/main/java/com/greatlearning/debate/StudentLab6Application.java
@@ -0,0 +1,14 @@
+package com.greatlearning.debate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentLab6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentLab6Application.class, args);
+		System.out.println("Welcome to Debate Competition");
+	}
+
+}
